e4104e35e30ca804e349634f8a9ae51cb7741142
jcore <julia.gavrilova@xcentium.com>
2020-09-09 21:16:34 -0400

    initial commit
    initial commit

---
+++ Symposium2020/www/js/sampleIndices.js
@@ -0,0 +1,20 @@
+// This file and it's content is automatically generated and should not be altered!
+var samples = [
+    [
+        {
+            "path": "www/world/00_Bike_Postcards/index.html",
+            "requiredFeatures": [
+                "image_tracking"
+            ],
+            "startupConfiguration": {
+                "camera_position": "back",
+                "camera_resolution": "auto"
+            }
+        }
+    ]
+]
+
+function getSamplePath(category, sample) {
+	return samples[category][sample];
+
+}
